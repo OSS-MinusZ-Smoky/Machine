65c63bfc02b0260a5cfae585edf0170f4efd94f6
Hyeong Chang Lee <hc9904@naver.com>
2018-09-28 02:15:58 +0900

    Update Machine.ino
    Add ping test

--- Machine.ino
+++ Machine.ino
@@ -1,23 +1,28 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
+#include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 #include <ESP8266HTTPClient.h>
 
-#define FREQUENCY 500 // send post frequency
-#define LED 13
+#define FREQUENCY 5000 // send post frequency 1=1ms
+#define ALALOGPIN A0 // MQ-7 Sensor 
 
-const char* ssid = "Happy_House"; // input your wifi ssid
-const char* password = "ab12345678"; // input your password
+const char* SSID = "AndroidHotspot5158"; // input your wifi ssid
+const char* PASSWORD = "ab12345678"; // input your password
 
-const String url = "http://192.168.0.2:3000/"; // input your server address
-const String data = String ("{\"hello\" : \"hello\"}"); // input your data
+const String URL = "http://192.168.43.225:3000/"; // input your server address
+
+
+ESP8266WebServer server(80);
+
+void pingCheck(){
+  server.send(200,"text/plain","im on!!!");
+}
 
 void setup(void){
-  pinMode(LED, OUTPUT);
-  digitalWrite(LED, 0);
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
+  WiFi.begin(SSID, PASSWORD);
   Serial.println("");
 
   // Wait for connection
@@ -25,26 +30,39 @@ void setup(void){
     delay(1000);
     Serial.print("Connecting to Wifi....\n");
   }
+
+  //Connection success
   Serial.println("");
   Serial.print("Connected to ");
-  Serial.println(ssid);
+  Serial.println(SSID);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+
+  server.on("/",pingCheck);
+  server.begin();
 }
 
 void loop(void){
+  int Sensor = analogRead(A0); // Read sensor value
+
+  //Send data
   HTTPClient http;
-  http.begin(url);
+  http.begin(URL);
   http.addHeader("Content-Type", "application/json");
+  String data = String ("{\"state\":\"")+String(Sensor)+String("\"}");   // input your datad
   int httpCode = http.POST(data);
   http.end();
+
+   
   if (httpCode = 200){
     Serial.print("Sended Data to ");
-    Serial.print(url);
+    Serial.print(URL);
+    Serial.print(" ");
     Serial.println(data);
   }
   else{
     Serial.println("Failed to send");
   }
   delay(FREQUENCY);
+  server.handleClient();
 }
